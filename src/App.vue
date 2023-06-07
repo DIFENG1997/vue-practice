75a0d23844f25a12161202278dde77e0d28b5edf
DIFENG1997 <hugodufourrire@outlook.com>
2023-06-07 19:25:36 +0800

    msg1

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+<!--     <element-view>
+
+    </element-view> -->
+    <emp-view>
+
+    </emp-view>
+  </div>
+</template>
+
+<script>
+/* import ElementView from './views/element/ElementView.vue' */
+import EmpView from './views/tlias/EmpView.vue'
+export default{
+
+components:{EmpView/* ElementView */}
+
+}
+</script>
+
+<style>
+
+</style>
